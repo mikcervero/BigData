66baed415881aad81ff493882572f985b6d40abc
micol <mik.cervero@gmail.com>
2020-05-01 09:38:07 +0200

    create job2

---
+++ Job2/src/main/java/BigData/Job2/App.java
@@ -0,0 +1,13 @@
+package BigData.Job2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
