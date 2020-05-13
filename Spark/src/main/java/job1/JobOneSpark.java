aff017947cc0755f70c3ab48743ae6f0ea32d5fb
fabiano <fabiano.dagostino96@gmail.com>
2020-05-13 17:36:53 +0200

    Bugfix Job1 Spark

--- Spark/src/main/java/job1/JobOneSpark.java
+++ Spark/src/main/java/job1/JobOneSpark.java
@@ -18,7 +18,7 @@ public class JobOneSpark {
 
 	public static void main(String[] args) {
 		
-		String file = "/home/fabiano/data/dataset.csv";
+		String file = "/home/fabiano/data/historical_stock_prices.csv";
 		
 		SparkSession spark = SparkSession
 				.builder()
@@ -33,11 +33,11 @@ public class JobOneSpark {
 		//considero record con anni compresi tra 2008 e 2018
 		JavaRDD<String[]> filtro = words.filter(x -> ((Integer.parseInt(DASH.split(x[7])[0])) >= 2008 && (Integer.parseInt(DASH.split(x[7])[0])) <=2018));
 	
-		//(ticker  ->    chiusura, data, volume, 1 (conteggio record))
-		JavaPairRDD<String, Double[]> tupla = filtro.mapToPair(x -> new Tuple2<>(x[0],new Double[] { Double.parseDouble(x[2]), transformDate(x[7]), Double.parseDouble(x[6]), 1.0}));
+		//(ticker  ->   chiusura, chiusura, chiusura, data, volume, 1 (conteggio record))
+		JavaPairRDD<String, Double[]> tupla = filtro.mapToPair(x -> new Tuple2<>(x[0],new Double[] {Double.parseDouble(x[2]),Double.parseDouble(x[2]), Double.parseDouble(x[2]), transformDate(x[7]), Double.parseDouble(x[6]), 1.0}));
 
 		//(ticker -> min_close, max_close, chiusurainiziale, chiusurafinale, somma_volume, conteggio record)
-		JavaPairRDD<String, Double[]> agg = tupla.reduceByKey((x,y)-> new Double[] {Math.min(x[0],y[0]), Math.max(x[0], y[0]), chiusurainiziale(x[0],y[0],x[1],y[1]), chiusurafinale(x[0],y[0],x[1],y[1]), x[2]+y[2], x[3]+y[3]});
+		JavaPairRDD<String, Double[]> agg = tupla.reduceByKey((x,y)-> new Double[] {Math.min(x[0],y[0]), Math.max(x[1], y[1]), chiusurainiziale(x[2],y[2],x[3],y[3]), chiusurafinale(x[2],y[2],x[3],y[3]), x[4]+y[4], x[5]+y[5]});
 		
 		//ticker, quotazione, min_close, max_close, media volumi tutto ordinato per quotazione decrescente
 		JavaRDD<String[]> ordinato = agg.map(couple -> new String [] {couple._1(), String.valueOf(Math.round((couple._2()[3]/couple._2()[2])*100-100)), String.valueOf(couple._2()[0]), String.valueOf(couple._2()[1]), String.valueOf((couple._2()[4])/(couple._2()[5]))}).sortBy(x -> Double.parseDouble(x[1]), false, 1);
