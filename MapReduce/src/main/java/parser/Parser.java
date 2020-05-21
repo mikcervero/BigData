cf97260240192218faa9c9fdb03c835a38a6800a
micol <mik.cervero@gmail.com>
2020-05-21 18:29:07 +0200

    aggiunti controlli nel parser

--- MapReduce/src/main/java/parser/Parser.java
+++ MapReduce/src/main/java/parser/Parser.java
@@ -1,59 +1,43 @@
 package parser;
 
-
-
-public class Parser 
-{
+public class Parser {
 	private final int TICKER = 0;
 	private final int NAME = 2;
-	
-	
-	public Parser() {};
-	
 
+	public Parser() {
+	};
 
-  public String processString (String text)	{
-	  
-	    String[] fields= text.split(",");
-		String ticker= fields[TICKER];
-		String sector= fields[fields.length-2];
-		String industry=fields[fields.length-1];
+	public String processString(String text) {
+
+		String[] fields = text.split(",");
+		String ticker = fields[TICKER];
+		String sector = fields[fields.length - 2];
+		String industry = fields[fields.length - 1];
 		String result = null;
 		
 		
 		
-        if(industry.indexOf('"')!=-1) {
-			
-			sector= fields[fields.length-3];
+		if (industry.equals("N/A") || ticker.equals("N/A") || sector.equals("N/A") ) {
+
+			return result;
 		}
 		
-		
-		
-		
-		if(sector.equals("N/A")) {
-			
-			return result; 
+
+		if (industry.indexOf('"') != -1) {
+
+			sector = fields[fields.length - 3];
 		}
-		
-		
-		
-				
-		if(fields[NAME].indexOf('"')!=-1) {
-			fields[NAME]=fields[2].replace('"', ' ');
-			fields[3]=fields[3].replace('"', ' ');
-			fields[NAME]=fields[2]+fields[3];
-	        
+
+
+		if (fields[NAME].indexOf('"') != -1) {
+			fields[NAME] = fields[2].replace('"', ' ');
+			fields[3] = fields[3].replace('"', ' ');
+			fields[NAME] = fields[2] + fields[3];
+
 		}
-		
-		
-				
-			 return result= ticker + "," + fields[NAME] + "," + sector ;
-			 	
-  }
-  
+
+		return result = ticker + "," + fields[NAME] + "," + sector;
+
+	}
 
 }
-    
-
-
-
